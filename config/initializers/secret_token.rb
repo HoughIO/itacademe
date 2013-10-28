07b21e03e9b6087ffabaa63c046b340a016cdc83
Graham Hough <graham.hough117@gmail.com>
2013-10-28 00:36:09 -0400

    First commit!
    So far have added twitter bootstrap functions and Static page holder
    (High_Voltage)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Itacademe::Application.config.secret_key_base = '387e60654e6cd047b8a9d4c7cee20fc74512c125121d8c6416b0f9a0ebc1dc925bbfc17dfe1fb79522a3dfb974544c72355141fa7fbf700211c5da52469bce84'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Itacademe::Application.config.session_store :cookie_store, key: '_itacademe_session'
